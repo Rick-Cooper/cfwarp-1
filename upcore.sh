652d84fab37c1253acd1f75c67f7ca20b678d6f0
jokenee <83391646+jokenee@users.noreply.github.com>
2021-09-13 21:12:01 +0800

    Update and rename updatcore.sh to upcore.sh

--- upcore.sh
+++ upcore.sh
@@ -27,7 +27,7 @@ function c7(){
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 yum install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm -y
 yum --enablerepo=elrepo-kernel install kernel-ml -y
-grub2-set-default 1
+grub2-set-default 0
 grub2-mkconfig -o /boot/grub2/grub.cfg
 reboot
 }
